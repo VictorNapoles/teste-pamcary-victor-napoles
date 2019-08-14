082d3ab01d1e8f341c9eba434694140cd94e25eb
Victor de Nápoles <victor.napoles-impulso@m4u.com.br>
2019-08-14 07:23:50 -0300

    Commit inicial.

---
+++ src/main/java/br/com/gpspamcary/testepamcaryvictornapoles/TestePamcaryVictorNapolesApplication.java
@@ -0,0 +1,13 @@
+package br.com.gpspamcary.testepamcaryvictornapoles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestePamcaryVictorNapolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestePamcaryVictorNapolesApplication.class, args);
+	}
+
+}
