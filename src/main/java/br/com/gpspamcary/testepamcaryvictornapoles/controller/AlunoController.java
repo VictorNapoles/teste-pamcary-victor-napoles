8300bd9ca8fcc88386e364f5dd5efd472f502ff0
Victor de Nápoles <victor.napoles-impulso@m4u.com.br>
2019-08-14 11:01:59 -0300

    Ajustes após testes.

--- src/main/java/br/com/gpspamcary/testepamcaryvictornapoles/controller/AlunoController.java
+++ src/main/java/br/com/gpspamcary/testepamcaryvictornapoles/controller/AlunoController.java
@@ -11,78 +11,67 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gpspamcary.testepamcaryvictornapoles.model.Aluno;
 import br.com.gpspamcary.testepamcaryvictornapoles.service.AlunoService;
 
+@RestController
+@RequestMapping("/aluno")
 public class AlunoController {
-	
+
 	@Autowired
 	private AlunoService service;
-	
+
 	@SuppressWarnings("rawtypes")
 	@GetMapping
 	public ResponseEntity obterTodos() {
-		
+
 		ResponseEntity<List<Aluno>> noContent = ResponseEntity.noContent().build();
-		return service.obterTodos().map((l)-> {
-			
-			if(l.isEmpty())
+		return service.obterTodos().map((l) -> {
+
+			if (l.isEmpty())
 				return noContent;
-			
+
 			return ResponseEntity.ok().body(l);
 		}).orElse(noContent);
 	}
-	
+
 	@SuppressWarnings("rawtypes")
 	@GetMapping("{id}")
-	public ResponseEntity obterPorId(@PathVariable("id")Long id) {
-		
-		return service.obterPorId(id).map((c)-> {
+	public ResponseEntity obterPorId(@PathVariable("id") Long id) {
+
+		return service.obterPorId(id).map((c) -> {
 			return ResponseEntity.ok().body(c);
 		}).orElse(ResponseEntity.noContent().build());
 	}
-	
+
 	@SuppressWarnings("rawtypes")
 	@PostMapping
 	public ResponseEntity inserir(@RequestBody Aluno aluno) {
-		
-		try {
-			aluno = service.inserir(aluno);
-			return new ResponseEntity<Aluno>(aluno, HttpStatus.CREATED);
 
-		} catch (Exception e) {
-			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		aluno = service.inserir(aluno);
+		return new ResponseEntity<Aluno>(aluno, HttpStatus.CREATED);
 
-		}
 	}
-	
+
 	@SuppressWarnings("rawtypes")
 	@PutMapping
 	public ResponseEntity alterar(@RequestBody Aluno aluno) {
-		
-		try {
-			aluno = service.alterar(aluno);
-			return new ResponseEntity<Aluno>(aluno, HttpStatus.OK);
 
-		} catch (Exception e) {
-			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		aluno = service.alterar(aluno);
+		return new ResponseEntity<Aluno>(aluno, HttpStatus.OK);
 
-		}
 	}
-	
+
 	@SuppressWarnings("rawtypes")
 	@DeleteMapping("{id}")
-	public ResponseEntity remover(@PathVariable("id")Long id) {
-		
-		try {
-			service.remover(id);
-			return new ResponseEntity<Aluno>(HttpStatus.OK);
+	public ResponseEntity remover(@PathVariable("id") Long id) {
 
-		} catch (Exception e) {
-			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		service.remover(id);
+		return new ResponseEntity<Aluno>(HttpStatus.OK);
 
-		}
 	}
 
 }
--- src/main/java/br/com/gpspamcary/testepamcaryvictornapoles/service/impl/AlunoServiceImpl.java
+++ src/main/java/br/com/gpspamcary/testepamcaryvictornapoles/service/impl/AlunoServiceImpl.java
@@ -3,19 +3,23 @@ package br.com.gpspamcary.testepamcaryvictornapoles.service.impl;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.gpspamcary.testepamcaryvictornapoles.model.Aluno;
 import br.com.gpspamcary.testepamcaryvictornapoles.repository.AlunoRepository;
 import br.com.gpspamcary.testepamcaryvictornapoles.service.AlunoService;
 
+@Service
 public class AlunoServiceImpl implements AlunoService {
 
 	@Autowired
 	private AlunoRepository repository;
 
 	@Override
-	public Optional<List<Aluno>>  obterTodos() {
+	public Optional<List<Aluno>> obterTodos() {
 		return Optional.ofNullable(repository.findAll());
 	}
 
@@ -26,6 +30,14 @@ public class AlunoServiceImpl implements AlunoService {
 
 	@Override
 	public Aluno alterar(Aluno aluno) {
+		Optional<Aluno> optAluno = Optional.ofNullable(aluno);
+		
+		Long id = optAluno.map(Aluno::getId).orElseThrow(() -> new EntityNotFoundException("Favor informar o id do aluno"));
+		
+		if (!repository.existsById(id)) {
+			throw new EntityNotFoundException("Aluno inexistente!");
+		}
+		
 		return repository.save(aluno);
 	}
 
